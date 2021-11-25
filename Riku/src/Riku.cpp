688cfe2a8049b3546636bf53184ce5452a826690
BridgeFan <kborycz@gmail.com>
2021-11-25 12:02:45 +0100

    Added darkening not active units

--- Riku/src/Riku.cpp
+++ Riku/src/Riku.cpp
@@ -293,7 +293,7 @@ void drawScene(Shader& lightingShader, Shader& lightCubeShader, float currentFra
 	lightingShader.setVec4("color_mod", 1.0f,1.0f,1.0f, 1.0f);
 	// render the loaded models
 	//draw tiles
-	auto& map = front::state.getMap();
+	const auto& map = front::state.getMap();
 	for(int i=0;i<map.size();i++)
 	{
 		for(int j=0;j<map[i].size();j++)
@@ -308,16 +308,24 @@ void drawScene(Shader& lightingShader, Shader& lightCubeShader, float currentFra
 				front::Object biomeObject = front::Object(front::biomeModels[map[i][j].biome.getName()],glm::vec3(i,(float)map[i][j].height*0.5f,j),glm::vec3(-90.0f,0.0f,180.0f),glm::vec3(0.1f,0.1f,0.1f));
 				biomeObject.Draw(lightingShader);
 			}*/
-			if(map[i][j].unit) {
-				front::Object unitObject = front::Object(front::unitModel,glm::vec3(i,(float)map[i][j].height*0.5f,j),glm::vec3(-90.0f,0.0f,180.0f),glm::vec3(0.01f,0.01f,0.01f));
-				unitObject.Draw(lightingShader);
-			}
 			if(map[i][j].object && front::objectModels.count(map[i][j].object->getName())) {
-				front::Object mapObject = front::Object(front::objectModels[map[i][j].object->getName()],glm::vec3(i,(float)map[i][j].height*0.5f,j),glm::vec3(-90.0f,0.0f,180.0f),glm::vec3(0.01f,0.01f,0.01f));
+				front::Object mapObject = front::Object(front::objectModels[map[i][j].object->getName()],glm::vec3(i,(float)map[i][j].height*0.5f,j),glm::vec3(-90.0f,0.0f,180.0f),glm::vec3(0.1f,0.1f,0.1f));
 				mapObject.Draw(lightingShader);
 			}
 		}
 	}
+	const auto& unit = front::state.getUnits();
+	for(int i=0;i<unit.size();i++) {
+		if(i!=front::focusedUnitIndex)
+			lightingShader.setVec4("color_mod", 0.7f,0.7f,0.7f, 1.0f);
+		int x = unit[i]->getMapX();
+		int y = unit[i]->getMapY();
+		front::Object object = front::Object(front::unitModel,glm::vec3(x,(float)map[x][y].height*0.5f,y),glm::vec3(-90.0f,0.0f,180.0f),glm::vec3(0.01f,0.01f,0.01f));
+		object.Draw(lightingShader);
+		lightingShader.setVec4("color_mod", 1.0f,1.0f,1.0f, 1.0f);
+
+	}
+
 	//return to default value
 	lightingShader.setVec4("color_mod", 1.0f,1.0f,1.0f, 1.0f);
 	//return to default value
