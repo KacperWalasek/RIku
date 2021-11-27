49646e00969adbb7436056f24aa246047f18d0d6
BridgeFan <kborycz@gmail.com>
2021-11-27 18:28:36 +0100

    Repaired path error

--- Riku/src/Frontend/Asset/AssetModel.cpp
+++ Riku/src/Frontend/Asset/AssetModel.cpp
@@ -9,7 +9,7 @@
 std::map<std::string, Model> front::AssetModel::models = {};
 
 front::AssetModel::AssetModel(std::string& path, Transform transform, float texScaleX, float texScaleY): transform(transform) {
-	std::string absolute = std::filesystem::absolute(path);
+	std::string absolute = std::filesystem::absolute(path).string();
 	if(models.find(absolute)==models.end()) {
 		models[absolute] = Model(path, texScaleX, texScaleY);
 	}
