e0ece33ca05babd0da0be2a47c41f95a90699f4e
KacperWalasek <41973501+KacperWalasek@users.noreply.github.com>
2022-01-15 20:19:10 +0100

    Pierwsza komunikacja

--- Riku/src/Riku.cpp
+++ Riku/src/Riku.cpp
@@ -9,10 +9,32 @@ int main() {
 	GameLogic logic("../assets", "../mini_assets");
 	front::Config config;
 	front::Frontend frontend(logic, config);
-	Network::WebModule webmodule;
-	webmodule.Init();
+	Network::WebModule::Init();
 	//webmodule.Invite();
-
+	bool inGame = false;
+	bool isHost = true;
+	std::string enemyIp = "192.168.0.14";
+	std::vector<std::string> invidedIps;
+	if(isHost)
+		Network::WebModule::Invite(enemyIp);
+	while (!inGame)
+	{
+		m_message message = Network::WebModule::ReceiveMessageStruct();
+		switch (message.type())
+		{
+		case MessType::Invitation:
+			if (!isHost && !message.empty())
+				Network::WebModule::AcceptInvitation(message.dataString());
+			break;
+		case MessType::InvitationAccepted:
+			if (isHost)
+				Network::WebModule::Join(message.dataString(), 1);
+			break;
+		default:
+			break;
+		}
+		
+	}
 	bool exitProgram = false;
 	while (!exitProgram)
 	{
