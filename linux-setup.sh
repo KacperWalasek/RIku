c95fe6e02e0a741e991c888199cd373c30db93b2
BridgeFan <kborycz@gmail.com>
2022-01-19 11:52:15 +0100

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Needed version of GCC: >=10
 
 #Needed packages:
@@ -11,19 +12,19 @@
 #libglm-dev (GL Math >=0.9)
 #libxi-dev (Lib Xi)
 
-sudo -i
-apt install libgl-dev
-apt install libglew-dev
-apt install libglfw3-dev
-apt install libassimp-dev
-apt install libjsoncpp-dev
-apt install liblua5.3-dev
-apt install libcegui-mk2-dev
-apt install libglm-dev
-apt install libxi-dev
+#sudo -i
+apt install -y libgl-dev
+apt install -y libglew-dev
+apt install -y libglfw3-dev
+apt install -y libassimp-dev
+apt install -y libjsoncpp-dev
+apt install -y liblua5.3-dev
+apt install -y libcegui-mk2-dev
+apt install -y libglm-dev
+apt install -y libxi-dev
 #install zmq
 echo 'deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/network:messaging:zeromq:release-stable.list
 curl -fsSL https://download.opensuse.org/repositories/network:messaging:zeromq:release-stable/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/network_messaging_zeromq_release-stable.gpg > /dev/null
-sudo apt update
-sudo apt install libzmq3-dev
-exit
+apt update
+apt install -y libzmq3-dev
+#exit
